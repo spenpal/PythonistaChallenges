ff603e32127474512d5b0a14bf929a224d2249d1
spenpal <spenpal2000@gmail.com>
2023-03-05 23:08:22 -0600

    add actual one-line

--- 2023/03/minesweeper_one_liner.py
+++ 2023/03/minesweeper_one_liner.py
@@ -1,4 +1,8 @@
 def minesweeper(grid: list[list[int]]) -> list[list[int]]:
+    return [[9 if grid[r][c] == 1 else sum(1 for i in range(max(0, r - 1), min(len(grid) - 1, r + 1) + 1) for j in range(max(0, c - 1), min(len(grid[0]) - 1, c + 1) + 1) if (i, j) != (r, c) and grid[i][j] == 1) for c in range(len(grid[0]))] for r in range(len(grid))]
+
+
+def minesweeper(grid: list[list[int]]) -> list[list[int]]:
     return [
         [
             9
