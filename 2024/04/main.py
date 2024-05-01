450ade2c36620ca6e7ba216c3eff627e825dac7f
spenpal <spenpal2000@gmail.com>
2024-05-01 09:19:07 -0500

    remove type-casting from str to int

--- 2024/04/main.py
+++ 2024/04/main.py
@@ -2,11 +2,11 @@ from typing import NamedTuple
 
 
 class Date(NamedTuple):
-    day: int
-    month: int
-    year: int
-    hour: int
-    minute: int
+    day: str
+    month: str
+    year: str
+    hour: str
+    minute: str
 
     def __lt__(self, other: "Date") -> bool:
         return (self.year, self.month, self.day, self.hour, self.minute) < (
@@ -18,15 +18,12 @@ class Date(NamedTuple):
         )
 
     def __str__(self) -> str:
-        return (
-            f"{self.day:02}-{self.month:02}-{self.year}_{self.hour:02}:{self.minute:02}"
-        )
+        return f"{self.day}-{self.month}-{self.year}_{self.hour}:{self.minute}"
 
 
-def sort_dates(dates: list[str], operations: list[str]) -> list[str]:  # noqa: PLR0912
+def sort_dates(dates: list[str], operations: list[str]) -> list[str]:
     dates = [
-        Date(*map(int, (date[:2], date[3:5], date[6:10], date[11:13], date[14:])))
-        for date in dates
+        Date(date[:2], date[3:5], date[6:10], date[11:13], date[14:]) for date in dates
     ]
 
     length = len(dates)
@@ -38,12 +35,12 @@ def sort_dates(dates: list[str], operations: list[str]) -> list[str]:  # noqa: P
             case "DESC":
                 dates.sort(reverse=True)
             case "UP":
-                month = int(op[1])
+                month = op[1]
                 for i in range(1, length):
                     if dates[i].month == month:
                         dates[i - 1], dates[i] = dates[i], dates[i - 1]
             case "DOWN":
-                day = int(op[1])
+                day = op[1]
                 i = 0
                 while i < length - 1:
                     if dates[i].day == day:
@@ -53,12 +50,12 @@ def sort_dates(dates: list[str], operations: list[str]) -> list[str]:  # noqa: P
                     else:
                         i += 1
             case "TOP":
-                year = int(op[1])
+                year = op[1]
                 for i in range(length):
                     if dates[i].year == year:
                         dates.insert(0, dates.pop(i))
             case "BOT":
-                year = int(op[1])
+                year = op[1]
                 bot_dates = []
                 curr_length = length
                 i = 0
